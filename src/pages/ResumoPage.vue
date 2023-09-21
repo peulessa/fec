f1ec32e391bbf6a169a7d23427f7629d778f8668
Pedro <pepeulessa@hotmail.com>
2023-09-21 10:50:36 -0300

    Alteração resumo

--- src/pages/ResumoPage.vue
+++ src/pages/ResumoPage.vue
@@ -11,20 +11,50 @@
 
     <q-separator inset />
 
-    <q-card-section>
-      <ul class="q-ma-none q-pa-none">
-        <li
-          v-for="servico in servicos"
-          class="q-pb-sm text-weight-thin flex justify-between"
-          style="list-style: none"
-        >
-          <div>
-            {{ servico }}
-          </div>
-        </li>
-      </ul>
+    <q-card-section class="flex justify-between">
+      <div>
+        <ul class="q-ma-none q-pa-none">
+          <li
+            v-for="servico in servicos"
+            class="q-pb-md text-weight-thin"
+            style="list-style: none"
+          >
+            <div>
+              {{ servico }}
+            </div>
+          </li>
+        </ul>
+      </div>
+
+      <div>
+        <ul class="q-ma-none q-pa-none">
+          <li
+            v-for="servicoPreco in servicosPreco"
+            class="q-pb-md"
+            style="list-style: none"
+          >
+            <div>
+              {{ servicoPreco }}
+            </div>
+          </li>
+        </ul>
+      </div>
     </q-card-section>
   </q-card>
+
+  <div class="q-px-md q-mt-md text-weight-thin">
+    <p>Total ({{ type }})</p>
+  </div>
+
+  <section class="flex justify-between" style="padding-top: 11em">
+    <q-btn
+      @click="back"
+      color="transparent"
+      text-color="primary"
+      label="Voltar"
+    />
+    <q-btn @click="next" color="primary" label="Concluir" />
+  </section>
 </template>
 
 <script setup>
@@ -33,19 +63,62 @@ import { ref } from "vue";
 const plano = sessionStorage.getItem("plano");
 const tipo = sessionStorage.getItem("planoTipo");
 const servicosUnformated = sessionStorage.getItem("servicos");
-const servicos = servicosUnformated.split(",");
+const servicosFormated = servicosUnformated.split(",");
 
-console.log(servicos);
+const servicos = [];
+const servicosPreco = [];
 
 let preco = ref("");
+let type = ref("");
 
-if (tipo === "Anual") {
+if (tipo === "Mensal") {
+  type = "Por Mês";
+} else {
+  type = "Por Ano";
+}
+
+servicosFormated.forEach((e) => {
+  if (e === "Online") {
+    servicos.push("Serviço Online");
+    if (tipo === "Mensal") {
+      servicosPreco.push("+ R$1/Mês");
+    } else {
+      servicosPreco.push("+ R$10/Ano");
+    }
+  }
+  if (e === "Armazenamento") {
+    servicos.push("Armazenamento Extra");
+    if (tipo === "Mensal") {
+      servicosPreco.push("+ R$2/Mês");
+    } else {
+      servicosPreco.push("+ R$12/Ano");
+    }
+  }
+  if (e === "Custom") {
+    servicos.push("Perfil Customizável");
+    if (tipo === "Mensal") {
+      servicosPreco.push("+ R$2/Mês");
+    } else {
+      servicosPreco.push("+ R$15/Ano");
+    }
+  }
+});
+
+if (tipo === "Mensal") {
   if (plano === "Arcade") {
-    preco = "R$10/Mês";
+    preco = "R$ 10/Mês";
   } else if (plano === "Advanced") {
-    preco = "R$12/Mês";
+    preco = "R$ 12/Mês";
   } else {
-    preco = "R$15/Mês";
+    preco = "R$ 15/Mês";
+  }
+} else {
+  if (plano === "Arcade") {
+    preco = "R$ 100/Ano";
+  } else if (plano === "Advanced") {
+    preco = "R$ 120/Ano";
+  } else {
+    preco = "R$ 150/Ano";
   }
 }
 </script>
